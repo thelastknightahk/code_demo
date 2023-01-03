057b2e484dd3ce8a8a98010fd52a2d81e09bac6b
Aung Prime <zaungk@arv.co.th>
2023-01-03 12:26:29 +0630

    design added

---
+++ lib/presentation/pages/main_page.dart
@@ -0,0 +1,30 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        items: [
+          Icon(Icons.home, size: 30),
+          Icon(Icons.add, size: 30),
+          Icon(Icons.person, size: 30),
+        ],
+        onTap: (index) {
+          //Handle button tap
+        },
+      ),
+    );
+  }
+}
